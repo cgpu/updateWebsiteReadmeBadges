8f77fbde6b68a0122ab658bc2ae562a53fcf4e7b
Josh Cook <joshuacook0023@gmail.com>
2019-09-18 08:26:21 -0400

    final of first draft
    Optional checks to ensure editing the correct line
    Add README for this package.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Update README Badges
+
+author: Josh Cook  
+date: 2019-09-18
+
+[![jhc github](https://img.shields.io/badge/GitHub-jhrcook-181717.svg?style=flat&logo=github)](https://github.com/jhrcook)
+[![jhc twitter](https://img.shields.io/badge/Twitter-@JoshDoesA-00aced.svg?style=flat&logo=twitter)](https://twitter.com/JoshDoesa)
+[![jhc website](https://img.shields.io/badge/Website-Joshua_Cook-5087B2.svg?style=flat&logo=telegram)](https://joshuacook.netlify.com)  
+[![R](https://img.shields.io/badge/-script-276DC3.svg?style=flat&logo=R)](https://cran.r-project.org)
+[![RStudio](https://img.shields.io/badge/RStudio-project-75AADB.svg?style=flat&logo=RStudio)](https://www.rstudio.com)
+
+A simple script to change my website GitHub badge to the new Academic Kickstarter website ([repo](https://github.com/jhrcook/academic-kickstart)).
+
+(Can also be re-purposed to change any other badge.)
--- updateWebsiteReadmeBadges.R
+++ updateWebsiteReadmeBadges.R
@@ -4,14 +4,16 @@ library(tidyverse)
 
 ## TARGET DIR
 # All README files in this directory (recursively) will be updated
-TARGET_DIR <- "~/Documents/Swift"
+TARGET_DIR <- "~/Documents/R"
 
-OLD_URL <- "https://www.joshdoesathing.com"
-NEW_URL <- "https://joshuacook.netlify.com"
+OLD_URL <- "https://twitter.com/JoshDoesa"
+NEW_URL <- "https://twitter.com/JoshDoesa"
 
-OLD_BADGE_TITLE <- "JoshDoesaThing"
-NEW_BADGE_TITLE <- "Joshua_Cook"
+OLD_BADGE_TITLE <- "Website-JoshDoesaThing"
+NEW_BADGE_TITLE <- "Twitter-@JoshDoesA"
 
+# additional specifications (optional)
+BADGE_LOGO <- "telegram"
 
 
 # get README file paths
@@ -21,18 +23,30 @@ readme_files <- readme_files[basename(readme_files) %in% c("README.txt", "README
 cat("Found the following files to update:\n")
 cat(readme_files, sep = "\n")
 
+
+
 # update the badge URL and title
 updateWebsiteReadmeBadges <- function(fn) {
     readme_lines <- readLines(fn)
     
     edit_website_badge <- function(old_line) {
-        new_line <- str_replace(old_line, OLD_URL, NEW_URL) %>%
-            str_replace(OLD_BADGE_TITLE, NEW_BADGE_TITLE)
-        return(new_line)
+        if (str_detect(old_line, paste0("logo=", str_to_lower(BADGE_LOGO)))) {
+            cat("file:", fn, "\n")
+            cat("--> OLD\n", old_line, "\n", sep = "")
+            new_line <- str_replace(old_line, OLD_URL, NEW_URL) %>%
+                str_replace(OLD_BADGE_TITLE, NEW_BADGE_TITLE)
+            cat("NEW -->\n", new_line, "\n\n", sep = "")
+            return(new_line)
+        } else {
+            return(old_line)
+        }
     }
     
     new_readme_lines <- lapply(readme_lines, edit_website_badge)
-    writeLines(unlist(new_readme_lines), fn)
+    a <- try(writeLines(unlist(new_readme_lines), fn))
+    if (class(a) == "try-error") {
+        warning(paste("unable to write to", fn))
+    }
     
 }
 
@@ -41,3 +55,4 @@ updateWebsiteReadmeBadges <- function(fn) {
 for (f in readme_files) {
     updateWebsiteReadmeBadges(f)
 }
+
